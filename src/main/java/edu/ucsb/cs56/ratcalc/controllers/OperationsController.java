a557f7112408d82437029d3e418f3d38ae1eaea3
Amanda Serex <amandaserex@ucsb.edu>
2020-01-28 17:52:43 -0800

    as - added controller method to show divide form

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -127,4 +127,28 @@ public class OperationsController {
         return "operations/multiply";
     }
 
+    @GetMapping("/divide")
+    public String getDivide(Model model) {
+        RatCalcForm ratCalcForm = new RatCalcForm();
+        ratCalcForm.setOp("x");
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/divide";
+    }
+
+    @GetMapping("/divide/results")
+    public String getDivisionResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult){
+        logger.info("getDivisionResult ratCalcForm=" +ratCalcForm);
+        ratCalcForm.setOp("X");
+
+        if(!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)) {
+            Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
+            Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
+            Rational result = Rational.quotient(r1,r2);
+            logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
+            ratCalcForm.setNumResult(result.getNumerator());
+            ratCalcForm.setDenomResult(result.getDenominator());
+    }
+    model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/divide";
+    }
 }
